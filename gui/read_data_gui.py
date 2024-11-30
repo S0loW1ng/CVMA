222c2a3c04d83078741a0f279582b3cf3c2eb712
spaceSpaceSpace2024 <mykhailo.samoilenko.github@gmail.com>
2024-11-30 14:29:41 -0500

    corrections

--- gui/read_data_gui.py
+++ gui/read_data_gui.py
@@ -1,8 +1,8 @@
-# Read Input json File
+# Read Input GUI .json
 import json
 import glob
 
-pattern = '*.json'
+pattern = 'data_gui_raw*.json'
 json_files = glob.glob(pattern)
 fid_input = json_files[-1]
 with open(fid_input, 'r') as file:
@@ -13,11 +13,11 @@ usernames_gui = data_gui['username']
 emails = emails_gui.split(",")
 usernames = usernames_gui.split(",")
 
-data_in = {
+data_gui_clean = {
     'usernames':usernames,
     'emails': emails,
     'first_name': data_gui['firstname'],
     'last_name': data_gui['lastname']}
 
-with open('data_in.json', 'w') as json_file:
-    json.dump(data_in, json_file, indent=4)
+# with open('data_gui_clean.json', 'w') as json_file:
+#     json.dump(data_gui_clean, json_file, indent=4)
