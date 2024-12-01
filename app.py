4061e9cb8518e25b04fc386cb8e60ebfbf793e67
Enrique <enrique@example.com>
2024-11-30 19:00:01 -0500

    Changed index.htm

--- app.py
+++ app.py
@@ -6,9 +6,10 @@ import requests
 import base64
 import re
 from leakcheck import LeakCheckAPI_v2
+from flask import Flask, render_template,send_from_directory
 
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder='gui')
 
 
 # Directory to store Sherlock results
@@ -82,13 +83,22 @@ def query_leakcheck(email, type):
     print("Printing leakcheck results")
     print(result)
     
+    print(email)
     #data = [{'source': {'name': 'lolzteam.net', 'breach_date': '2019-02', 'unverified': 0, 'passwordless': 1, 'compilation': 0}, 'username': 'retr0', 'email': 'rus.nevka@gmail.com', 'fields': ['username', 'email']}, {'password': 'A12345', 'source': {'name': 'DayZ.com', 'breach_date': '2016-03', 'unverified': 0, 'passwordless': 0, 'compilation': 0}, 'country': 'US', 'ip': '75.141.220.164', 'username': 'Retr0', 'email': 'dope_man_420@hotmail.com', 'fields': ['password', 'country', 'ip', 'username', 'email']}, {'password': '3177389', 'source': {'name': 'WiiHacks.com', 'breach_date': '2013-08', 'unverified': 0, 'passwordless': 0, 'compilation': 0}, 'ip': '81.103.114.35', 'username': 'retr0', 'email': 'dopey1467@gmail.com', 'fields': ['password', 'ip', 'username', 'email']}, {'password': 'vuth3', 'source': {'name': 'GamerzPlanet.net', 'breach_date': '2015-09', 'unverified': 0, 'passwordless': 0, 'compilation': 0}, 'ip': '76.23.79.233', 'username': 'retr0', 'email': 'prodigy.zach@gmail.com', 'fields': ['password', 'ip', 'username', 'email']}, {'password': 'Zelda99', 'source': {'name': 'Konduit', 'breach_date': '2020-04', 'unverified': 0, 'passwordless': 0, 'compilation': 0}, 'username': 'Retr0', 'email': 'jkimball1993@gmail.com', 'fields': ['password', 'username', 'email']}, {'password': 'howella6764', 'source': {'name': 'FFShrine.org', 'breach_date': '2015-09', 'unverified': 0, 'passwordless': 0, 'compilation': 0}, 'username': 'Retr0', 'email': 'oldzkoolz@yahoo.com', 'fields': ['password', 'username', 'email']}, {'password': 'Mamamia10', 'source': {'name': 'Mpgh.net', 'breach_date': '2015-10', 'unverified': 0, 'passwordless': 0, 'compilation': 0}, 'ip': '188.22.77.214', 'username': 'ReTr0', 'email': 'christoph.linzner@gmx.at', 'fields': ['password', 'ip', 'username', 'email']}, {'password': 'kentut', 'source': {'name': 'gPotato.com', 'breach_date': '2006-11', 'unverified': 0, 'passwordless': 0, 'compilation': 0}, 'username': 'retr0', 'email': 'dead_in_head@hotmail.com', 'fields': ['password', 'username', 'email']}, {'password': 'mtvqm7g9', 'source': {'name': 'PESGaming.com', 'breach_date': None, 'unverified': 0, 'passwordless': 0, 'compilation': 0}, 'ip': '62.254.64.14', 'username': 'retr0', 'email': 'jones_ufc@hotmail.com', 'fields': ['password', 'ip', 'username', 'email']}, {'password': 'kokelio', 'source': {'name': 'Exvagos.com', 'breach_date': '2022-07', 'unverified': 0, 'passwordless': 0, 'compilation': 0}, 'dob': '1000-01-01', 'username': 'retr0', 'email': 'kokelio@hotmail.com', 'fields': ['password', 'dob', 'username', 'email']}]
     #data = [{'source': {'name': 'Deezer.com', 'breach_date': '2019-09', 'unverified': 0, 'passwordless': 1, 'compilation': 0}, 'country': 'CA', 'dob': '1986-09-30', 'first_name': 'Mykhailo', 'last_name': 'Samoilenko', 'username': 'Mykhailo..Samoilenko', 'email': 'smykhailo@yahoo.com', 'fields': ['country', 'dob', 'first_name', 'last_name', 'username', 'email']}]
     return result
 
 
+@app.route('/')
+def serve_index():
+    return app.send_static_file('index.html')
+
+
+
+
 @app.route('/GetInformation', methods=['POST'])
 def get_information():
+    print(request.get_json())
     passwordList = []
     addressList = []
     usernameList = []
@@ -105,9 +115,16 @@ def get_information():
 
     jsonInput = request.get_json()
     print("Checking usernames")
+
+    print("Full JSON Input:", jsonInput)
+    print("Type of 'usernames':", type(jsonInput['usernames']))
+    print("Content of 'usernames':", jsonInput['usernames'])
+
     
 
-    for username in jsonInput["usernames"]:
+    for username in jsonInput['usernames']:
+        print(username)
+        print(jsonInput)
         for entry in query_leakcheck(username, "username"):
             if 'email' in entry and entry['email'] != 'N/A' and entry['email'] not in emailList:
                 emailList.append(entry['email'])
@@ -143,7 +160,7 @@ def get_information():
     
     print("Runing sherlock ")
     print("skiping sherlock ")
-    #accountFoundList.append(run_sherlock(jsonInput["usernames"], "sherlockOutput"))
+    accountFoundList.append(run_sherlock(jsonInput["usernames"], "sherlockOutput"))
     print("Returning data")
     json_joint_data = json.dumps(data, indent=5)
 
